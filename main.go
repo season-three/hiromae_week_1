37c636a1ae1b77bcc9f3bc364b3997e15e981289
HirokiMaekawa1 <mkhk.31.ne@gmail.com>
2020-05-29 18:17:00 +0900

    update

--- main.go
+++ main.go
@@ -4,21 +4,42 @@ import (
 	"fmt"
 
 	"github.com/PuerkitoBio/goquery"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+
 )
 
 func main() {
 	url := "https://go-tour-jp.appspot.com/welcome/1"
 
-	doc, err := goquery.NewDocument(url) //①HTTPリクエスト行を入れる
+	//httpアクセス
+	doc, err := goquery.NewDocument(url)
 	if err != nil {
-		fmt.Printf("%s\n", err) //③万が一のエラーのためのコードを入れる
+		fmt.Printf("%s\n", err)
 	}
 
-	title, err := doc.Find("title").Text()
+	//タイトル取ってくる
+	title := doc.Find("title").Text()
+	titleresult := "ページタイトル:" + title
+	fmt.Println(titleresult)
+
+
+	//DB接続
+	db, err := gorm.Open("mysql", "scraper:11194222@tcp(localhost:3306)/scraping?charset=utf8&parseTime=true&loc=Asia%2FTokyo")
 	if err != nil {
-		fmt.Printf("%s\n", err) //③万が一のエラーのためのコードを入れる
+		fmt.Printf("%s\n", err)
+	}
+	defer db.Close()
+
+
+	//構造体の作成
+	type HPtitle struct {
+		id      int
+		content string
 	}
 
-	fmt.Println("ページタイトル:" + title)
+
+	//データの追加
+	db.Create（"titleresult"）
 
 }
